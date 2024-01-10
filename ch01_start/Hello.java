27485323d385df4c7b2979e8a6e9231c332223ca
Seroy <jkspop12@naver.com>
2024-01-10 16:33:22 +0900

    practice

---
+++ ch01_start/Hello.java
@@ -0,0 +1,17 @@
+package ch01_start;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// 콘솔 출력
+		// 각 코드의 종료는 ; 세미콜론
+		System.out.println("Hello World");
+//		main 코드 실행 ctrl + F11 , Run as + java application
+		//라인 복사 ctrl + alt + 아래키
+		// ctrl + d 라인삭제
+		}
+	// alt + (위/아래) 이동
+	// 클래스명은 대문자
+	//crtl + alt + 아래방향
+
+}
